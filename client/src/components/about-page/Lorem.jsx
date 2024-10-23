39671d6b136dc99a2496c95421327e2dfa97e192
MuhammadAmirSaeed <amirsaeed.gcuf@gmail.com>
2024-10-23 20:05:10 +0500

    Refactor font styling and layout in ContactSales and Hero components

--- client/src/components/about-page/Lorem.jsx
+++ client/src/components/about-page/Lorem.jsx
@@ -5,7 +5,7 @@ function Lorem() {
     <>
       <div className='lg:p-[100px] xl:p-[100px] p-[30px] flex flex-col justify-center items-center'>
       <p className=' text-[40px] leading-[50px] expo-medium text-center'>What is Lorem Ipsum?</p>
-     <div className='flex items-center justify-center mt-[15px] max-w-[1100px] w-full'> <div className=' max-w-[1100px] w-full text-[16px] leading-[26px] font-[400] font-[Poppins] lg:text-center xl:text-center text-center'>{"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their. It is a long established fact that reader will be distracted by the readable content of a page when looking at its layout. "}</div>
+     <div className='flex items-center justify-center mt-[15px] max-w-[1130px] w-full'> <div className=' max-w-[1130px] w-full text-[16px] leading-[26px] font-[400] font-[Poppins] lg:text-center xl:text-center text-center'>{"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their. It is a long established fact that reader will be distracted by the readable content of a page when looking at its layout. "}</div>
 </div>
 
       </div>
