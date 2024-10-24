341c377152684fa30f6adf67cf834d5204bbfe9c
MuhammadAmirSaeed <amirsaeed.gcuf@gmail.com>
2024-10-25 01:25:24 +0500

    Refactor font styling and leading in FifthSection, FourthSection, and SixthSection components

--- client/src/components/software/FifthSection.jsx
+++ client/src/components/software/FifthSection.jsx
@@ -14,7 +14,7 @@ function FifthSection() {
       
        {/* text */}
        <div className='flex flex-col justify-center gap-4'  data-aos="zoom-in-right" data-aos-delay="400">
-        <p className='max-w-[542px] text-[30px] sm:text-[40px] leading-[50px] font-[500]'>We Can Help You Save Time And Money in Your Business</p>
+        <p className='max-w-[542px] text-[30px] sm:text-[40px] leading-[40px] md:leading-[50px] font-[500]'>We Can Help You Save Time And Money in Your Business</p>
         <p className='max-w-[494px] text-[16pxpx] leading-[23px] font-[400] font-[Poppins]'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
         <p className='max-w-[494px] text-[16pxpx] leading-[23px] font-[400] font-[Poppins]'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
        </div>
